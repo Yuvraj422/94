3157085eae59828e2018104eeae2a65006111cc6
Yuvraj422 <72796144+Yuvraj422@users.noreply.github.com>
2022-04-15 12:09:49 +0530

    Add files via upload

---
+++ expo config.js
@@ -0,0 +1,12 @@
+// F// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAOSCrpwimqunImQV3F07H9OdDLX4UlEVk",
+    authDomain: "fauly-232e3.firebaseapp.com",
+    databaseURL: "https://fauly-232e3-default-rtdb.firebaseio.com",
+    projectId: "fauly-232e3",
+    storageBucket: "fauly-232e3.appspot.com",
+    messagingSenderId: "255101541622",
+    appId: "1:255101541622:web:b0c74dfae91efa8ed460d7",
+    measurementId: "G-RCPG02VG2S"
+  };
